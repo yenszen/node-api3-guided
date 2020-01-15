c687d7ddf66909c4d3933ea3093a10ed6fa8ee55
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-15 10:40:24 +0100

    wip

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet'); // importing a pre-baked piece of middleware
-
 const hubsRouter = require('./hubs/hubs-router.js'); // [ ARRAY OF MIDDLEWARES, EACH BEING AN ENDPOINT ]
+
+function auth(req, res, next) {
+  // 1- we need to check username and password
+  // 2- username and password are in the request somewhere
+  // 3- credentials will be expected in the req.body { username, password }
+  // 4- if username === 'emma' & password === '1234' then proceed ---> next()
+  // 5- otherwise we res.json 400 and that's that 
+}
 
 const server = express();
 
+
 server.use(express.json()); // ADDING A SEGMENT TO THE TUBE -> PLUGGING A PIECE OF MIDDLEWARE
+server.use(auth); // this would be putting auth across the board
+
 
 // let's add a segment that obscures the fact that this is an Express app
 // helmet, as imported, is a function that returns a middleware (which is in turn a function)
