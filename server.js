cbf10fac640ed5ddbfcb94a44adb84fa5a45a4db
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-15 10:31:25 +0100

    wip

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet'); // importing a pre-baked piece of middleware
 
 const hubsRouter = require('./hubs/hubs-router.js'); // [ ARRAY OF MIDDLEWARES, EACH BEING AN ENDPOINT ]
 
@@ -7,6 +8,8 @@ const server = express();
 server.use(express.json()); // ADDING A SEGMENT TO THE TUBE -> PLUGGING A PIECE OF MIDDLEWARE
 
 // let's add a segment that obscures the fact that this is an Express app
+// helmet, as imported, is a function that returns a middleware (which is in turn a function)
+server.use(helmet( /* we could potentially add helmet configuration */ ))
 
 server.use('/api/hubs', hubsRouter); // ADDING SEVERAL MIDDLEWARES
 
