2bc7279db589f08c6d4140f701387c87f02d58b3
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-05-12 10:49:09 +0800

    completed first lecture segment

--- server.js
+++ server.js
@@ -1,20 +1,49 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(logger);
+// server.use(lockOut);
+server.use(addName);
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+function logger(req, res, next) {
+  console.log(req.method);
+  next();
+}
+
+function lockOut(req, res, next) {
+  const sec = new Date().getSeconds();
+
+  if (sec % 3 === 0) {
+    res.status(403).json({ message: "api is down" });
+  } else {
+    next();
+  }
+}
+
+function addName(req, res, next) {
+  if (!req.name) {
+    req.name = "Cassandra";
+  }
+  next();
+}
 
 module.exports = server;
